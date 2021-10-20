57c6913457a8453c55f8784ae4bc0025271cadd1
Nciklol <mr.awesome2006hi@gmail.com>
2021-10-19 23:32:07 -0500

    no

--- pages/invite.tsx
+++ pages/invite.tsx
@@ -8,7 +8,7 @@ const Invite: NextPage = () => {
     <Head>
         <title>Invite • YBot</title>
         <meta property="og:title" content="Invite YBot" data-react-helmet="true" />
-        <meta httpEquiv="refresh" content="0;URL=https://discord.com/api/oauth2/authorize?client_id=480926911095111682&permissions=294498200822&scope=bot" data-react-helmet="true"/>
+        <meta httpEquiv="refresh" content="0;URL=https://google.com" data-react-helmet="true"/>
     </Head>
 
     return (
