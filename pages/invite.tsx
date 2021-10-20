1413e1310277c74e7b40fb9932ef3bf727727a17
Nciklol <mr.awesome2006hi@gmail.com>
2021-10-19 23:35:25 -0500

    I'm an idiot

--- pages/invite.tsx
+++ pages/invite.tsx
@@ -4,15 +4,17 @@ import Head from "next/head";
 const baseURL = "https://discord.com/api/oauth2/authorize?client_id=480926911095111682&permissions=294498200822&scope=bot"
 
 const Invite: NextPage = () => {
-
-    <Head>
-        <title>Invite • YBot</title>
-        <meta property="og:title" content="Invite YBot" data-react-helmet="true" />
-        <meta httpEquiv="refresh" content="0;URL=https://google.com" data-react-helmet="true"/>
-    </Head>
-
     return (
+        
+        <div>
+                <Head>
+                    <title>Invite • YBot</title>
+                    <meta property="og:title" content="Invite YBot" data-react-helmet="true" />
+                    <meta httpEquiv="refresh" content={"0;URL="+baseURL} data-react-helmet="true"/>
+                </Head>
+
         <u><a href={baseURL}>Didn&apos;t get redirected? Click on this text.</a></u>
+        </div>
     )
 }
 
